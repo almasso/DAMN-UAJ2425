98f17fa31be443915a9f1b4ab5a1bb16aed81654
roiqd03 <roiquint@ucm.es>
2025-04-22 17:26:40 +0200

    TrackerAssets

---
+++ DAMN/src/Damn/ProgressionTracker.cpp
@@ -0,0 +1,28 @@
+#include "ProgressionTracker.h"
+#include "TrackerEvent.h"
+#include "Persistence.h"
+
+bool ProgressionTracker::accept(TrackerEvent* trackerEvent)
+{
+    std::string s = trackerEvent->GetEventType();
+    if (isLevelStarted) {
+        if (s == "LevelEnd") {
+            isLevelStarted = false;
+            return true;
+        }
+    }
+    else {
+        if (s == "LevelStart") {
+            isLevelStarted = true;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool ProgressionTracker::process(Persistence* persistence, TrackerEvent* trackerEvent)
+{
+    persistence->QueueEvent(trackerEvent);
+    persistence->ForceFlush();
+    return true;
+}
